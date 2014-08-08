c7eddacf8cb73146a112dbab3b5df6cdcf1a42b5
Eric Siegel <siegel.eric@gmail.com>
2014-08-07 23:19:03 -0700

    add research link

--- src/ui/mainWindow.cpp
+++ src/ui/mainWindow.cpp
@@ -10,7 +10,7 @@ namespace ui {
       QMessageBox::about(this, "ABOUT",
                          tr("<h1>Image resizing using content-aware seamcarving</h1>"
                             "<p>This is a work in progress and only supports removing seams.</p>"
-                            "<p>More info: http://www.faculty.idc.ac.il/arik/SCWeb/imret/index.html</p>"));
+                            "<p><a href='http://www.faculty.idc.ac.il/arik/SCWeb/imret/index.html'>More info</a></p>"));
    }
 
    void MainWindow::image_opened(QSize size) {
